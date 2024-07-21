6f1be14c83af4225023d0cb4da91f5f628402472
clown <tj1211keynote@outlook.com>
2024-07-21 18:52:26 +0800

    feat: auto registry service

--- cmd/logservice/main.go
+++ cmd/logservice/main.go
@@ -6,17 +6,24 @@ import (
 	stlog "log"
 
 	"github.com/tClown11/distributed-demo/log"
+	"github.com/tClown11/distributed-demo/registry"
 	"github.com/tClown11/distributed-demo/service"
 )
 
 func main() {
 	log.Run("./distributed.log")
 	host, port := "localhost", "4000"
+	serviceAddress := fmt.Sprintf("http://%s:%s", host, port)
+
+	r := registry.Registration{
+		ServiceName: "Log Service",
+		ServiceURL:  serviceAddress,
+	}
 	ctx, err := service.Start(
 		context.Background(),
-		"Log Service",
 		host,
 		port,
+		r,
 		log.RegisterHandlers,
 	)
 	if err != nil {
--- service/service.go
+++ service/service.go
@@ -5,15 +5,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/tClown11/distributed-demo/registry"
 )
 
-func Start(ctx context.Context, serviceName, host, port string, registerHandlersFunc func()) (context.Context, error) {
+func Start(ctx context.Context, host, port string, reg registry.Registration, registerHandlersFunc func()) (context.Context, error) {
 	registerHandlersFunc()
-	ctx = startService(ctx, serviceName, host, port)
+	ctx = startService(ctx, reg.ServiceName, host, port)
+
+	err := registry.RegisterService(reg)
+	if err != nil {
+		return ctx, err
+	}
 	return ctx, nil
 }
 
-func startService(ctx context.Context, serviceName, host, port string) context.Context {
+func startService(ctx context.Context, serviceName registry.ServiceName, host, port string) context.Context {
 	ctx, cancel := context.WithCancel(ctx)
 	var srv http.Server
 	srv.Addr = ":" + port
